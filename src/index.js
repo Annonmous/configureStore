b5834e5d08d661d7ee3abbd31ecc843adb4344c8
Annonmous <mhuzaifahkha397@gmail.com>
2021-12-22 16:34:09 +0500

    unknown bhund

--- src/index.js
+++ src/index.js
@@ -3,10 +3,28 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {configureStore} from '@reduxjs/toolkit' 
+import { Provider } from 'react-redux';
+
+const InitialState={
+
+  name:"Huzaifah",
+  age:21,
+  gender:"male"
+
+}
+const store=configureStore({
+reducer:(state)=>{
+  return state
+},
+  preloadedState:InitialState
+})
 
 ReactDOM.render(
   <React.StrictMode>
+    <Provider store={store}>
     <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
